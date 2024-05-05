09379a8f437df7e37e9d931c6ede0b80235678b3
Alfa <104553605+theimmortall@users.noreply.github.com>
2024-05-05 12:11:37 +0530

    Update prims.cpp

--- prims.cpp
+++ prims.cpp
@@ -1,91 +1,66 @@
-
 #include <iostream>
+#include <limits.h>
 using namespace std;
-int minKey(int key[], bool mstSet[], int V)
+int min_vertex(int distance[], int no_of_vertices, bool visited[20])
 {
-	int min = INT_MAX, min_index;
-
-	for (int v = 0; v < V; v++)
-		if (mstSet[v] == false && key[v] < min)
-			min = key[v], min_index = v;
-
+	int min_v = INT_MAX, min_index;
+	for (int i = 0; i < no_of_vertices; i++)
+	{
+		if (distance[i] < min_v && visited[i] == false)
+		{
+			min_v = distance[i];
+			min_index = i;
+		}
+	}
 	return min_index;
 }
-void printMST(int parent[], int graph[20][20], int V)
+void prims_algo(int graph[20][20], int no_of_vertices)
 {
-	cout << "Edge \tWeight\n";
-	for (int i = 1; i < V; i++)
-		cout << parent[i] << " - " << i << " \t"
-			<< graph[i][parent[i]] << " \n";
-}
-
-void primMST(int graph[20][20], int V)
-{
-	
-	int parent[V];
-
-	
-	int key[V];
-
-	// To represent set of vertices included in MST
-	bool mstSet[V];
-
-	// Initialize all keys as INFINITE
-	for (int i = 0; i < V; i++)
-		key[i] = INT_MAX, mstSet[i] = false;
-
-	// Always include first 1st vertex in MST.
-	// Make key 0 so that this vertex is picked as first
-	// vertex.
-	key[0] = 0;
-
-	// First node is always root of MST
-	parent[0] = -1;
-
-	// The MST will have V vertices
-	for (int count = 0; count < V - 1; count++) {
-		
-		// Pick the minimum key vertex from the
-		// set of vertices not yet included in MST
-		int u = minKey(key, mstSet, V);
-
-		// Add the picked vertex to the MST Set
-		mstSet[u] = true;
-
-		// Update key value and parent index of
-		// the adjacent vertices of the picked vertex.
-		// Consider only those vertices which are not
-		// yet included in MST
-		for (int v = 0; v < V; v++)
-
-			// graph[u][v] is non zero only for adjacent
-			// vertices of m mstSet[v] is false for vertices
-			// not yet included in MST Update the key only
-			// if graph[u][v] is smaller than key[v]
-			if (graph[u][v] && mstSet[v] == false
-				&& graph[u][v] < key[v])
-				parent[v] = u, key[v] = graph[u][v];
+	bool visited[no_of_vertices];
+	int parent[no_of_vertices], distance[no_of_vertices];
+	for (int i = 0; i < no_of_vertices; i++)
+	{
+		distance[i] = INT_MAX;
+		visited[i] = false;
 	}
-
-	// Print the constructed MST
-	printMST(parent, graph, V);
+	distance[0] = 0;
+	parent[0] = -1;
+	for (int i = 0; i < no_of_vertices - 1; i++)
+	{
+		int u = min_vertex(distance, no_of_vertices, visited);
+		visited[u] = true;
+		for (int v = 0; v < no_of_vertices; v++)
+		{
+			if (!visited[v] && graph[u][v] && distance[v] > graph[u][v])
+			{
+				parent[v] = u;
+				distance[v] = graph[u][v];
+			}
+		}
+	}
+	cout << "Source\t"
+		 << "Destination\t"
+		 << "Distance" << endl;
+	for (int i = 0; i < no_of_vertices; i++)
+	{
+		cout << parent[i] << "\t" << i << "\t" << graph[i][parent[i]] << endl;
+	}
 }
-
-// Driver's code
 int main()
 {
-	int V;
-    cout<<"Enter the number of vertices:";
-    cin>>V;
-    int graph[20][20];
-    cout<<"Enter the adjacency matrix of the graph"<<endl;
-    for(int i=0;i<V;i++){
-        for(int j=0;j<V;j++){
-            cout<<"G["<<i<<","<<j<<"]";
-            cin>>graph[i][j];
-        }
-    }
-	primMST(graph, V);
-
+	int no_of_vertices;
+	cout << "Enter the number of vertices : ";
+	cin >> no_of_vertices;
+	int graph[20][20];
+	cout << "Enter the adjacency matrix of the graph" << endl;
+	for (int i = 0; i < no_of_vertices; i++)
+	{
+		for (int j = 0; j < no_of_vertices; j++)
+		{
+			cout << "G[" << i << "][" << j << "] : ";
+			cin >> graph[i][j];
+		}
+	}
+	prims_algo(graph, no_of_vertices);
 	return 0;
 }
